fd9a795752960572e78313f7d99e91a14995734d
Your Name <you@example.com>
2022-12-30 04:23:41 +0530

    AA

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 const express = require('express');
 const app = express();
 const { mongoClient, MongoClient } = require('mongodb');
